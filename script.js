ee676965a374d97c12eafe0679709b20bc61d909
Kabina Thapa <thapakabina28@gmail.com>
2023-11-29 16:23:10 -0500

    feat:implement dynamic updating of displayed value based on slider position

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 const pageviews=document.getElementById('pageviews')
 const price=document.getElementById('price')
 const rangeinput=document.getElementById('rangeinput')
-price.innerHTML=rangeinput.value
-rangeinput.addEventListener('input', function(){
-    price.innerHTML=rangeinput.value
-})
+const priceMapping={
+   '1':{
+        price:'$8',
+        pageviews:'10K'
+
+    },
+    2:{
+        price:'$12',
+        pageviews:'50K'
+
+    },
+    3:{
+        price:'$16',
+        pageviews:'100K'
+
+    },
+    4:{
+        price:'$24',
+        pageviews:'500K'
+
+    },
+    5:{
+        price:'$36',
+        pageviews:'1M'
+
+    }
+}
+console.log(priceMapping[rangeinput.value])
+function updatePrice(){
+    selectValue=priceMapping[rangeinput.value]
+    price.innerHTML=selectValue.price
+    pageviews.innerHTML=selectValue.pageviews
+    }
+    updatePrice()
+rangeinput.addEventListener('input',updatePrice)
