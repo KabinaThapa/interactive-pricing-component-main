ba0318b43dd09b9db90868db14f3c5b8a19c44d6
Kabina Thapa <thapakabina28@gmail.com>
2023-11-29 18:18:09 -0500

    feat:add logic to update pricing based on the selected billing, for yearly display 25%discount on price

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
 const pageviews=document.getElementById('pageviews')
 const price=document.getElementById('price')
 const rangeinput=document.getElementById('rangeinput')
+const togglerange=document.getElementById('togglerange')
 const priceMapping={
    '1':{
-        price:'$8',
+        price:'8',
         pageviews:'10K'
 
     },
     2:{
-        price:'$12',
+        price:'12',
         pageviews:'50K'
 
     },
     3:{
-        price:'$16',
+        price:'16',
         pageviews:'100K'
 
     },
     4:{
-        price:'$24',
+        price:'24',
         pageviews:'500K'
 
     },
     5:{
-        price:'$36',
+        price:'36',
         pageviews:'1M'
 
     }
 }
 console.log(priceMapping[rangeinput.value])
-function updatePrice(){
-    selectValue=priceMapping[rangeinput.value]
-    price.innerHTML=selectValue.price
+function updatePriceandViews(){
+    const selectValue=priceMapping[rangeinput.value]
+    const yearlybillingValue=togglerange.value==='2'
+    const discount=yearlybillingValue? 0.25 * parseFloat(selectValue.price):0
+    const discountedPrice=parseFloat(selectValue.price)-discount
+    console.log(selectValue)
+    price.innerHTML=`$${discountedPrice}`
     pageviews.innerHTML=selectValue.pageviews
     }
-    updatePrice()
-rangeinput.addEventListener('input',updatePrice)
+    updatePriceandViews()
+rangeinput.addEventListener('input',updatePriceandViews)
+togglerange.addEventListener('input', updatePriceandViews)
